13590e228f807366e64ef2a077df0d732e8e09a1
JaviVLAB <javier.zen@gmail.com>
2024-05-13 20:55:23 +0200

    update 13 may

--- README.md
+++ README.md
@@ -7,48 +7,53 @@
 Installation on mac or linux
 
 ```
-cd backend/
-python3.11 -m venv venv
-source venv/bin/activate
-pip install "fastapi[all]"
-pip install -r requirements.txt
+% cd backend/
+% python3.11 -m venv venv
+% source venv/bin/activate
+(venv)% pip install "fastapi[all]"
+(venv)% pip install -r requirements.txt
 ```
 ## Frontend
 
 ```
-npm install
+% cd nextjsfrontend
+% npm install
 ```
 
-## Execution
+## ComfyUI
+
+- Install ComfyUI (https://stable-diffusion-art.com/how-to-install-comfyui/)
+- Install ComfyUI Manager (https://github.com/ltdrdata/ComfyUI-Manager)
+- Install Was Node (in ComfyUI Manager)
+- Install missing nodes (in ComfyUI Manager)
+- Install Models IC-light (in ComfyUI Manager)
+
+# Execution
+
+## Backend
 
 ```
-uvicorn main:app --reload
+% cd backend
+% source venv/bin/activate
+(venv)% uvicorn main:app --reload
 ```
 
+## Frontend
+
+```
+% npm run dev
+``` 
 
 
 
-
-# Importante
-
-# Backend
-
-[] 
-
-# Frontend
-
-
-
-
-Instalar ComfyUI
-Instalar ComfyUI Manager
-Instalar Was Node
-Instalar missing nodes
-Instalar Models IC-light
-
+## ComfyUI
 
 
 ```
-cd /Users/javi/Documents/workspace/ComfyUI/ComfyUI
+cd /ComfyUI/ComfyUI
 python3.11 main.py
 ```
+
+# Usage
+
+Open the APP at [http://localhost:3000/prototipo](localhost:3000/prototipo)
---
+++ nextjsfrontend/app/prototipo/page.js
@@ -0,0 +1,65 @@
+"use client"
+import Head from 'next/head';
+import { useEffect, useRef, useState } from 'react';
+
+export default function Home() {
+    const staticImageSrc = '/Rembrandt.jpg';
+    const [imgSrc, setImageSrc] = useState(staticImageSrc);
+    const websocketRef = useRef(null);
+
+    useEffect(() => {
+        
+        const websocket = new WebSocket('ws://localhost:8000/ws');
+        websocketRef.current = websocket;
+        websocket.onopen = () => {
+            console.log("WebSocket connection established.");
+        };
+
+        websocket.onmessage = (event) => {
+            const message = event.data
+
+            if (message === 'No people detected') {
+                setImageSrc(staticImageSrc);
+
+            } else {
+
+                const src = `data:image/jpeg;base64,${message}`;
+                setImageSrc(src);
+            }
+        };
+
+        websocket.onerror = (error) => {
+            console.log("WebSocket error:", error);
+        };
+
+        websocket.onclose = () => {
+            console.log("WebSocket connection closed.");
+        };
+
+        return () => {
+            if (websocketRef.current) {
+                websocketRef.current.close();
+            }
+        };
+    }, []);
+
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <Head>
+        <title>Imagen Centrada con Botones</title>
+        <meta name="description" content="Web app con Next.js que muestra una imagen y botones" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-1 flex-col items-center justify-center">
+      <img src={imgSrc} alt="Webcam Stream" 
+            style={{ width: "400", height: "600" }} />
+      <div className="mt-5">
+          <button className="mx-2 px-4 py-2 text-lg cursor-pointer">Boton 1</button>
+          <button className="mx-2 px-4 py-2 text-lg cursor-pointer">Boton 2</button>
+        </div>
+      </main>
+    </div>
+  );
+}
