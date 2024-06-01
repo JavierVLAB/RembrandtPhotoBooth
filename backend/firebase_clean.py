9a72870682be2af6efbc184d541ca910cb2552b8
JaviVLAB <javier.zen@gmail.com>
2024-06-01 14:16:42 +0200

    Update firebase_clean.py

--- backend/firebase_clean.py
+++ backend/firebase_clean.py
@@ -3,7 +3,7 @@ import firebase_admin
 from firebase_admin import credentials, storage
 import datetime
 
-cred = credentials.Certificate('../auth/testapi-4ea72-firebase-adminsdk-thkcf-6e888c94f2.json')
+cred = credentials.Certificate('./auth/testapi-4ea72-firebase-adminsdk-thkcf-6e888c94f2.json')
 firebase_admin.initialize_app(cred, {
     'storageBucket': 'testapi-4ea72.appspot.com'
 })
@@ -13,12 +13,14 @@ bucket = storage.bucket()
 blobs = bucket.list_blobs()
 
 now = datetime.datetime.now(datetime.timezone.utc)
-one_day_ago = now - datetime.timedelta(days=1)
+one_day_ago = now - datetime.timedelta(hours=14)
 
+counter = 0
 
 for blob in blobs:
 
     if blob.time_created < one_day_ago:
         blob.delete()
+        counter += 1
 
-print("Imagenes borradas en firebase")
+print(str(counter) + " imagenes borradas en firebase")
