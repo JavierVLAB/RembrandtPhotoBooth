564fc2f68bbf0041b5a16e43b4f1b4df1656d8a8
JaviVLAB <javier.zen@gmail.com>
2025-03-23 17:47:14 +0100

    upload the new firebase connection

--- backend/firebase_clean.py
+++ backend/firebase_clean.py
@@ -3,9 +3,9 @@ import firebase_admin
 from firebase_admin import credentials, storage
 import datetime
 
-cred = credentials.Certificate('./auth/testapi-4ea72-firebase-adminsdk-thkcf-6e888c94f2.json')
+cred = credentials.Certificate('auth/iedcampus-firebase-adminsdk-fbsvc-48b145a483.json')
 firebase_admin.initialize_app(cred, {
-    'storageBucket': 'testapi-4ea72.appspot.com'
+    'storageBucket': 'iedcampus.firebasestorage.app'
 })
 
 bucket = storage.bucket()
@@ -13,14 +13,14 @@ bucket = storage.bucket()
 blobs = bucket.list_blobs()
 
 now = datetime.datetime.now(datetime.timezone.utc)
-one_day_ago = now - datetime.timedelta(hours=14)
+one_day_ago = now - datetime.timedelta(hours=14) # 14 horas es buen numero para borrar lo de ayer, pero no lo de hoy
 
 counter = 0
 
-for blob in blobs:
-
-    if blob.time_created < one_day_ago:
-        blob.delete()
-        counter += 1
+for blob in blobs: 
+    if counter == 0:
+        if blob.time_created > one_day_ago:
+            blob.delete()
+            counter += 1
 
 print(str(counter) + " imagenes borradas en firebase")
