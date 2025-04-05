1a7f6ed360954e8119b3ed3d795409c3e81374d5
Rembrandt <javier.zen@gmail.com>
2025-04-05 14:29:57 +0200

    Revert "upload the new firebase connection"
    This reverts commit 564fc2f68bbf0041b5a16e43b4f1b4df1656d8a8.

--- backend/firebase_clean.py
+++ backend/firebase_clean.py
@@ -3,9 +3,9 @@ import firebase_admin
 from firebase_admin import credentials, storage
 import datetime
 
-cred = credentials.Certificate('auth/iedcampus-firebase-adminsdk-fbsvc-48b145a483.json')
+cred = credentials.Certificate('./auth/testapi-4ea72-firebase-adminsdk-thkcf-6e888c94f2.json')
 firebase_admin.initialize_app(cred, {
-    'storageBucket': 'iedcampus.firebasestorage.app'
+    'storageBucket': 'testapi-4ea72.appspot.com'
 })
 
 bucket = storage.bucket()
@@ -13,14 +13,14 @@ bucket = storage.bucket()
 blobs = bucket.list_blobs()
 
 now = datetime.datetime.now(datetime.timezone.utc)
-one_day_ago = now - datetime.timedelta(hours=14) # 14 horas es buen numero para borrar lo de ayer, pero no lo de hoy
+one_day_ago = now - datetime.timedelta(hours=14)
 
 counter = 0
 
-for blob in blobs: 
-    if counter == 0:
-        if blob.time_created > one_day_ago:
-            blob.delete()
-            counter += 1
+for blob in blobs:
+
+    if blob.time_created < one_day_ago:
+        blob.delete()
+        counter += 1
 
 print(str(counter) + " imagenes borradas en firebase")
